8cf4a55224e0adc308acc813f6fac037722b08f5
feldt <alcid3srz@gmail.com>
2018-03-20 17:25:50 -0600

    index & create

---
+++ SammyAuto/Controllers/ServiceTypeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SammyAuto.Data;
+
+namespace SammyAuto.Models
+{
+    public class ServiceTypeController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public ServiceTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //get: servicesTypes
+        public IActionResult Index()
+        {
+            return View(_db.ServiceTypes.ToList());
+        }
+
+        //get: serviceType/create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post: services/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ServiceType serviceTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(serviceTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serviceTypes);
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _db.Dispose();
+        }
+    }
+}
---
+++ SammyAuto/Models/IndividualButtonPartial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SammyAuto.Models
+{
+    public class IndividualButtonPartial
+    {
+        public string ButtonType { get; set; }
+        public String Action { get; set; }
+        public String Glyph { get; set; }
+        public String Text { get; set; }
+
+        public int? ServiceId { get; set; }
+
+        public String ActionParametres
+        {
+
+            get
+            {
+                var parameter = new StringBuilder(@"/");
+                if (ServiceId != null && ServiceId != 0)
+                {
+                    parameter.Append(String.Format("{0}", ServiceId));
+
+                }
+
+                return parameter.ToString().Substring(0, parameter.Length);
+            }
+        }
+    }
+}
